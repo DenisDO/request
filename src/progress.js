310d927879db0ae377350ee99edf719f9b1c06c7
Denis Derkach <podstavka128@gmail.com>
2019-01-26 23:12:52 +0200

    feature/progress, add onDownloadProgress function and edit event listeners

--- src/progress.js
+++ src/progress.js
@@ -1,4 +1,5 @@
 const uploadBar = document.getElementById('uploadProgress-bar');
+const downloadBar = document.getElementById('downloadProgress-bar');
 
 function onUploadProgress(event, isFinished) {
   const percentage = Math.round(event.loaded / event.total * 100);
@@ -12,52 +13,35 @@ function onUploadProgress(event, isFinished) {
   }
 }
 
-// function onDownloadProgress(event) {
-//   console.warn('onDownloadProgress');
-//   console.info(event.loaded, ' / ', event.total);
-//   console.warn('onDownloadProgress');
-// }
+function onDownloadProgress(event, isFinished) {
+  const percentage = Math.round(event.loaded / event.total * 100);
+  downloadBar.style.width = `${percentage}%`;
 
-// document.getElementById('uploadForm').onsubmit = function(e) {
-//   e.preventDefault();
-//   const form = new FormData();
-//   const myHeaders = new Headers();
-//   myHeaders.append('Content-Type', 'multipart/form-data');
-//   form.append('sampleFile', e.target.sampleFile.files[0]);
-//   //   fetch('http://localhost:8000/upload', {
-//   //     method: 'POST',
-//   //     body: form
-//   //   });
+  if (isFinished) {
+    downloadBar.style.backgroundColor = '#38d160';
+  }
+}
 
-//   const xhr = new HttpRequest({ baseUrl: 'http://localhost:8000' });
-//   xhr.post('/upload', { onUploadProgress, data: form });
-// };
+document.getElementById('uploadForm').onsubmit = function(e) {
+  e.preventDefault();
+  const form = new FormData();
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'multipart/form-data');
+  form.append('sampleFile', e.target.sampleFile.files[0]);
 
-// document.getElementById('downloadForm').onsubmit = function(e) {
-//   e.preventDefault();
+  // eslint-disable-next-line no-undef
+  const xhr = new HttpRequest({ baseUrl: 'http://localhost:8000' });
+  xhr.post('/upload', { onUploadProgress, data: form });
+};
 
-//   const xhr = new HttpRequest({ baseUrl: 'http://localhost:8000' });
-//   xhr.get('/files/basketball.png', { onDownloadProgress });
-// };
+document.getElementById('downloadForm').onsubmit = function(e) {
+  e.preventDefault();
 
-// function transformResponse(text) {
-//     console.log('transformResponse');
-//     console.log(text + '\n_____________________________');
-//     console.log('transformResponse');
-// }
-
-// function transformRequest() {
-//     console.log('transformRequest');
-// }
-
-// const reuest = new HttpRequest({
-//   baseUrl: 'http://localhost:8000'
-// });
-
-// reuest.get('/form', { transformResponse })
-//   .then(response => {
-//     console.log(response);
-//   })
-//   .catch(e => {
-//     console.log(e);
-//   });
+  // eslint-disable-next-line no-undef
+  const xhr = new HttpRequest({ baseUrl: 'http://localhost:8000' });
+  xhr.get(`/files/${e.target[0].value}`, { responseType: 'arraybuffer', onDownloadProgress })
+    .then(data => {
+      const imageURL = window.URL.createObjectURL(new Blob([new Uint8Array(data)], { type: 'image/jpeg' }));
+      document.getElementById('myImage').src = imageURL;
+    });
+};
